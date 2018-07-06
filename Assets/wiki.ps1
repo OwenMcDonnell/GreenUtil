b0fff09d12372b794ae8fdec2f2d353d6588e39f
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-07-06 09:21:44 -0700

    Update wiki.ps1

--- Assets/wiki.ps1
+++ Assets/wiki.ps1
@@ -5,18 +5,17 @@ $mdFolder = "C:\projects\wiki2"
 Write-Host "Start publishing documentation to wiki"
 git config --global credential.helper store
 Add-Content "$HOME\.git-credentials" "https://$($token):x-oauth-basic@github.com`n"
-git config --global user.email "leandro.ltavares@gmail.com"
-git config --global user.name "Leandro Luciani Tavares"
+git config --global user.email "owenmcdonnell@appveyor.com"
+git config --global user.name "Owen McDonnell"
 Remove-Item $mdFolder -Force -Recurse -ErrorAction SilentlyContinue
 md $mdFolder
 cd $mdFolder
 git --version
 choco install -y --limitoutput git
 git --version
-git clone --branch=master "https://github.com/leandroltavares/GreenUtil.wiki.git" -q
+git clone --branch=master "https://github.com/owenmcdonell/GreenUtil.wiki.git" -q
 cd GreenUtil.wiki
-#git clone --branch=master "https://github.com/leandroltavares/netdeveloperpraticum.wiki.git" -q
-#cd netdeveloperpraticum.wiki
+
 git status
 #C:\projects\greenutil\Assets\MarkdownGenerator.exe "C:\projects\greenutil\GreenUtil\bin\Release\net471\GreenUtil.dll" "$($mdFolder)\GreenUtil.wiki"
 #git status
